587aef9d12faf59845722289aafccdc038bd975f
Alexey Maslov <alexey.y.maslov@gmail.com>
2016-04-07 00:16:30 +0300

    added test.

--- enw_test.go
+++ enw_test.go
@@ -1,6 +1,8 @@
 package enw
 
 import (
+	"runtime"
+
 	. "gopkg.in/check.v1"
 	"testing"
 )
@@ -10,3 +12,13 @@ func Test(t *testing.T) { TestingT(t) }
 type EnwSuite struct{}
 
 var _ = Suite(&EnwSuite{})
+
+func (e *EnwSuite) TestStartAndStopWatching(c *C) {
+	n := runtime.NumGoroutine()
+	Watch("VAR", func(string, string) {})
+	c.Assert(runtime.NumGoroutine(), Equals, n+1)
+
+	Forget("VAR")
+	runtime.Gosched()
+	c.Assert(runtime.NumGoroutine(), Equals, n)
+}
